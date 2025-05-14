0cb1c96b0c7bcf2790b56b400b1cfb9871786f87
Jun Rey Ansing <junrey.o.ansing@gmail.com>
2025-05-15 07:56:18 +0800

    Implemented exception if file does not exist in the Github page

--- github_download_test.py
+++ github_download_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 from pathlib import Path
 
@@ -18,31 +19,38 @@ downloads = Path("D:/Downloads")
 # Iterate through list of filenames
 for file in filenames:
 
+    # Attempt downloading the file and verify if downloaded successfully
     try:
-        # 1. Open the Github page of downloadable PDF file and wait for 5 seconds
+        # Open the Github page of downloadable PDF file and wait for 5 seconds
         driver.get("https://github.com/ffisk/books/blob/master/" + file)
         time.sleep(5)
 
-        # 2. Find the download button
+        # Find the download button
         download_button = driver.find_element(By.XPATH, '//*[@id=":rgr:"]/button')
-        
-        # 3. Click download button and wait then for 10 seconds in downloading the file
-        download_button.click()
-        time.sleep(10)
 
-        # 4. Get the full directory of the target file from Downloads folder
-        pdf_file = downloads / file
+        # Check if the download button is found and displayed on the page
+        if download_button and download_button.is_displayed():
+            print("https://github.com/ffisk/books/blob/master/" + file + " exist.")
 
-        # 5. Check if the file was successfully downloaded
-        if pdf_file.exists():
-            print(f"✅ '{pdf_file.name}' exists in Downloads.")
-        else:
-            print(f"❌ '{pdf_file.name}' not found in Downloads.")
+            # Click download button and wait then for 10 seconds in downloading the file
+            download_button.click()
+            time.sleep(10)
 
+            # Get the full directory of the target file from Downloads folder
+            pdf_file = downloads / file
 
-    except AssertionError:
-        print("Test failed: 'OpenAI' not found in the page title.")
+            # Check if the file was successfully downloaded
+            if pdf_file.exists():
+                print(f"✅ '{pdf_file.name}' downloaded successfully in Downloads folder.")
+                print()
+            else:
+                print(f"❌ '{pdf_file.name}' not found in Downloads.")
+                print()
 
-    # finally:
-# 6. Close the browser
+    # If the file does not exist in the Github page
+    except NoSuchElementException:
+        print("https://github.com/ffisk/books/blob/master/" + file + " does not exist.")
+        print()
+
+# Close the browser
 driver.quit()
